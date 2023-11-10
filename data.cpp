5e9277bbb7d624e5fa77dc940648511feccce7c0
AnastasiayA26 <75423962+AnastasiayA26@users.noreply.github.com>
2023-11-10 20:00:27 +0300

    Update data.cpp

--- data.cpp
+++ data.cpp
@@ -12,6 +12,15 @@ enum VisualizationType {
     VECTORS
 };
 
+struct PolarDataPoint {
+    double radius, angle;
+};
+
+struct CoordinateLimits {
+    double maxRadius;
+    double maxAngle;
+};
+
 std::string getTypeName(VisualizationType visType) {
     switch (visType) {
         case POINTS:
@@ -25,45 +34,63 @@ std::string getTypeName(VisualizationType visType) {
     }
 }
 
-std::vector<std::pair<double, double>> generatePoints(int numPoints) {
-    std::vector<std::pair<double, double>> data;
+std::vector<PolarDataPoint> generatePointsWithNoise(int numPoints, const CoordinateLimits& limits, double noiseLevel) {
+    std::vector<PolarDataPoint> data;
 
     for (int i = 0; i < numPoints; ++i) {
-        double r = 5.0 * (static_cast<double>(rand()) / RAND_MAX);
-        double theta = 2.0 * M_PI * (static_cast<double>(rand()) / RAND_MAX);
-        data.emplace_back(r, theta);
+        PolarDataPoint point;
+        point.radius = static_cast<double>(rand()) / RAND_MAX * limits.maxRadius;
+        point.angle = static_cast<double>(rand()) / RAND_MAX * limits.maxAngle;
+
+        point.radius += noiseLevel * static_cast<double>(rand()) / RAND_MAX;
+        point.angle += noiseLevel * static_cast<double>(rand()) / RAND_MAX;
+
+        data.push_back(point);
     }
 
     return data;
 }
 
-std::vector<std::pair<double, double>> generateArcs(int numArcs) {
-    std::vector<std::pair<double, double>> data;
+std::vector<PolarDataPoint> generateArcsWithNoise(int numArcs, const CoordinateLimits& limits, double noiseLevel) {
+    std::vector<PolarDataPoint> data;
 
     for (int i = 0; i < numArcs; ++i) {
-        double r = 5.0 * (static_cast<double>(rand()) / RAND_MAX);
-        double theta1 = 2.0 * M_PI * (static_cast<double>(rand()) / RAND_MAX);
-        double theta2 = 2.0 * M_PI * (static_cast<double>(rand()) / RAND_MAX);
-        data.emplace_back(r, theta1);
-        data.emplace_back(r, theta2);
+        PolarDataPoint start, end;
+        start.radius = static_cast<double>(rand()) / RAND_MAX * limits.maxRadius;
+        start.angle = static_cast<double>(rand()) / RAND_MAX * limits.maxAngle;
+        end.radius = static_cast<double>(rand()) / RAND_MAX * limits.maxRadius;
+        end.angle = static_cast<double>(rand()) / RAND_MAX * limits.maxAngle;
+
+        start.radius += noiseLevel * static_cast<double>(rand()) / RAND_MAX;
+        start.angle += noiseLevel * static_cast<double>(rand()) / RAND_MAX;
+        end.radius += noiseLevel * static_cast<double>(rand()) / RAND_MAX;
+        end.angle += noiseLevel * static_cast<double>(rand()) / RAND_MAX;
+
+        data.push_back(start);
+        data.push_back(end);
     }
 
     return data;
 }
 
-std::vector<std::pair<double, double>> generateVectors(int numVectors) {
-    std::vector<std::pair<double, double>> data;
+std::vector<PolarDataPoint> generateVectorsWithNoise(int numVectors, const CoordinateLimits& limits, double noiseLevel) {
+    std::vector<PolarDataPoint> data;
 
     for (int i = 0; i < numVectors; ++i) {
-        double r = 5.0 * (static_cast<double>(rand()) / RAND_MAX);
-        double theta = 2.0 * M_PI * (static_cast<double>(rand()) / RAND_MAX);
-        data.emplace_back(r, theta);
+        PolarDataPoint point;
+        point.radius = static_cast<double>(rand()) / RAND_MAX * limits.maxRadius;
+        point.angle = static_cast<double>(rand()) / RAND_MAX * limits.maxAngle;
+
+        point.radius += noiseLevel * static_cast<double>(rand()) / RAND_MAX;
+        point.angle += noiseLevel * static_cast<double>(rand()) / RAND_MAX;
+
+        data.push_back(point);
     }
 
     return data;
 }
 
-void writeToFile(const std::vector<std::pair<double, double>>& data, const std::string& filename) {
+void writeToFile(const std::vector<PolarDataPoint>& data, const std::string& filename) {
     std::ofstream file(filename);
 
     if (file.is_open()) {
@@ -71,7 +98,7 @@ void writeToFile(const std::vector<std::pair<double, double>>& data, const std::
         file << std::fixed << std::setprecision(2);
 
         for (const auto& point : data) {
-            file << point.first << " " << point.second << std::endl;
+            file << point.radius << " " << point.angle << std::endl;
         }
 
         file.close();
@@ -80,19 +107,18 @@ void writeToFile(const std::vector<std::pair<double, double>>& data, const std::
     }
 }
 
-std::vector<std::pair<double, double>> generateData(int numObjects, VisualizationType visType) {
+std::vector<PolarDataPoint> generateDataWithNoise(int numObjects, VisualizationType visType, const CoordinateLimits& limits, double noiseLevel) {
     if (visType == POINTS) {
-        return generatePoints(numObjects);
+        return generatePointsWithNoise(numObjects, limits, noiseLevel);
     } else if (visType == ARCS) {
-        return generateArcs(numObjects);
+        return generateArcsWithNoise(numObjects, limits, noiseLevel);
     } else if (visType == VECTORS) {
-        return generateVectors(numObjects);
+        return generateVectorsWithNoise(numObjects, limits, noiseLevel);
     } else {
         std::cerr << "Invalid visualization type" << std::endl;
         return {};
     }
 }
-
 
 bool isNumber(const std::string& str) {
     std::istringstream iss(str);
@@ -103,16 +129,17 @@ bool isNumber(const std::string& str) {
 int main(int argc, char* argv[]) {
     srand(static_cast<unsigned int>(time(nullptr)));
 
-    if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " <visType> <numObjects>" << std::endl;
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <visType> <numObjects> <noiseLevel>" << std::endl;
         return 1;
     }
 
     std::string visTypeString = argv[1];
     std::string numObjectsString = argv[2];
+    std::string noiseLevelString = argv[3];
 
-    if (!isNumber(numObjectsString)) {
-        std::cerr << "Invalid input for numObjects: " << numObjectsString << std::endl;
+    if (!isNumber(numObjectsString) || !isNumber(noiseLevelString)) {
+        std::cerr << "Invalid input for numObjects or noiseLevel" << std::endl;
         return 1;
     }
 
@@ -122,6 +149,8 @@ int main(int argc, char* argv[]) {
         std::cerr << "Error: numObjects exceeds the maximum allowed value (1000000)" << std::endl;
         return 1;
     }
+
+    double noiseLevel = std::stod(noiseLevelString);
 
     VisualizationType visType;
 
@@ -136,13 +165,18 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    std::string typeName = getTypeName(visType);
-    std::vector<std::pair<double, double>> data = generateData(numObjects, visType);
+    CoordinateLimits limits;
+    limits.maxRadius = 10.0;
+    limits.maxAngle = 2.0 * M_PI;
 
-    writeToFile(data, typeName + "_data.txt");
+    std::string typeName = getTypeName(visType);
+    std::vector<PolarDataPoint> dataWithNoise = generateDataWithNoise(numObjects, visType, limits, noiseLevel);
+
+    writeToFile(dataWithNoise, typeName + "_data.txt");
 
     return 0;
 }
+
 
 
 
