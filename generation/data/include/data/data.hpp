036618ccef87c02b10393bc9491e6c7f928a499a
AnastasiayA26 <75423962+AnastasiayA26@users.noreply.github.com>
2023-12-25 23:47:16 +0300

    Update data.hpp

--- generation/data/include/data/data.hpp
+++ generation/data/include/data/data.hpp
@@ -8,17 +8,24 @@
 #include <random>
 #include <vector>
 
+struct Point {
+    double phi;
+    double theta;
+
+    Point(double phi, double theta) : phi(phi), theta(theta) {}
+};
+
 class SphericalCoordinatesGenerator {
 public:
-    void writePointsToFile(const std::string &filename, const std::vector<std::vector<double>> &points) const;
+    void writePointsToFile(const std::string& filename, const std::vector<Point>& points) const;
 
-    std::vector<std::vector<double>> readPointsFromFile(const std::string &filename) const;
+    std::vector<Point> readPointsFromFile(const std::string& filename) const;
 
-    std::vector<std::vector<double>> generatePointsOnArc(int numPoints, double phi_start, double theta_start, double phi_end, double theta_end) const;
+    std::vector<Point> generatePointsOnArc(int numPoints, double phi_start, double theta_start, double phi_end, double theta_end) const;
 
-    std::vector<std::vector<double>> addCoordinateNoiseOnSphere(const std::vector<std::vector<double>> &points, double noiseLevel) const;
+    std::vector<Point> addCoordinateNoiseOnSphere(const std::vector<Point>& points, double noiseLevel) const;
 
-    std::vector<std::vector<double>> generatePointsFromCenter(int numArcs, int numPointsPerArc, double centerPhi, double centerTheta, double phi_start, double theta_start, double phi_end, double theta_end) const;
+    std::vector<Point> generatePointsFromCenter(int numArcs, int numPointsPerArc, double centerPhi, double centerTheta, double phi_start, double theta_start, double phi_end, double theta_end) const;
 };
 
 #endif // DATA_HPP
