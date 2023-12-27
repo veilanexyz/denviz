496e78506bd29e99053bb75de9ffb1433496d0c9
IrinaDudko0712 <113918542+IrinaDudko0712@users.noreply.github.com>
2023-12-27 21:55:14 +0300

    Update visualization.hpp

--- generation/visualization/include/visualization/visualization.hpp
+++ generation/visualization/include/visualization/visualization.hpp
@@ -1,31 +1,23 @@
 #pragma once
 #ifndef VISUALIZATION_HPP
 #define VISUALIZATION_HPP
-#include <vtkSphereSource.h>
-#include <vtkPolyDataMapper.h>
-#include <vtkProperty.h>
-#include <vtkPointData.h>
-#include <vtkSTLReader.h>
-#include <vtkSTLWriter.h>
-#include <vtkTexture.h>
-#include <vtkFloatArray.h>
-#include <vtkPNGReader.h>
-#include <vtkPNGWriter.h>
-#include <vtkCamera.h>
-#include <vtkWindowToImageFilter.h>
-#include <vtkRenderer.h>
-#include <vtkRenderWindow.h>
-#include <vtkRenderWindowInteractor.h>
-#include <vtkMath.h>
-#include <iostream>
+
+#include <vtkSmartPointer.h>
+#include <vtkActor.h>
 #include <vector>
 #include "data/data.hpp"
 
 class Visualization
 {
 public:
-        vtkSmartPointer<vtkActor> generateSphereWithPoints(const std::vector<Point>& points, double opacity, double color[3], double radius, double sphereColor[4]) const;
-        vtkSmartPointer<vtkActor> createSphere(double radius, double color[3]) const;
-        void Mapping(const std::string& imagePath, const std::string& stlPath = "output.stl") const;
+
+    vtkSmartPointer<vtkActor> generateSphereWithPoints(const std::vector<Point>& points, double opacity, double color[3], double radius) const;
+
+    vtkSmartPointer<vtkActor> createSphere(double radius, double color[3]) const;
+
+    vtkSmartPointer<vtkActor> createRealSphere(double radius, double color[3], double opacity) const;
+
+    vtkSmartPointer<vtkActor> Mapping(const std::string& imagePath, double radius, const double color[3], double opacity) const;
 };
+
 #endif
